f418622e9870ff95d88584f89ebe7d210f1cc709
Florence Comajes <renztoygwapo@gmail.com>
2022-08-18 13:24:48 +0800

    validate ip request

--- src/Domain/IP/Data/IpData.php
+++ src/Domain/IP/Data/IpData.php
@@ -2,9 +2,12 @@
 namespace Domain\IP\Data;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\Validation\IP;
+use Spatie\LaravelData\Attributes\Validation\Max;
 
 class IpData extends Data
 {
     public string $label;
+    #[Max(15), IP]
     public string $ip;
 }
