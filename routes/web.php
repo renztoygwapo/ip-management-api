ff9c586bb6130e7f198c8f346ac891169f63a453
Florence Comajes <renztoygwapo@gmail.com>
2022-08-18 11:02:08 +0800

    set up api mode only

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['IP Management' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
